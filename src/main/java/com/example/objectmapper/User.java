895cb000441704d40ebb0b7908e0b0b78c8b5c50
uni <bsy1482@naver.com>
2022-02-11 17:42:32 +0900

    text -> object 다시 볼 필요 있음

---
+++ src/main/java/com/example/objectmapper/User.java
@@ -0,0 +1,50 @@
+package com.example.objectmapper;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+
+    private String name;
+    private int age;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    public User(){
+        this.name = null;
+        this.age = 0;
+        this.phoneNumber = null;
+    }
+
+    public User(String name, int agem, String phoneNumber){
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // objectMapper를 사용하는 class에서는 get이름인 method를 사용하면 안된다. (get,set 메소드 제외)
+    public User DefaultUser(){
+        return new User("default",0,"1232465");
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -0,0 +1,36 @@
+package com.example.objectmapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectMapperApplicationTests {
+
+    @Test
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("------------");
+
+        // objectMapper?
+        // Text JSON -> Object
+
+        // controller req json(text) -> object
+        // response object -> json(text)
+
+        var objectMapper = new ObjectMapper();
+
+        // object -> text
+        // object mapper get method를 활용
+        var user = new User("uni",20,"010-111-2555");
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+
+        // text -> object
+        // object mapper 는 default 생성자를 필요로 한다.
+        var objectUser = objectMapper.readValue(text, User.class);
+        System.out.println(objectMapper);
+    }
+
+}
